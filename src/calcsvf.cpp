6469d81c38dfeb5480a710d5300e66c1ea977e25
Lei Zhang (Student) <lzhan253@sundevils.asu.edu>
2018-03-19 17:39:24 -0700

    uupdate 3-19

--- src/calcsvf.cpp
+++ src/calcsvf.cpp
@@ -9,7 +9,18 @@ int main(int argc, char** argv) {
 	fopen_s(&file_out, "output.bin", "w+");
 	unsigned char *data = (unsigned char *)malloc(512 * 512 * sizeof(unsigned char));
 	decompress("./test.bin", data, 512*512);
-	inf(file_in, file_out);*/
+	inf(file_in, file_out);
+
+	char *tempBuffer = new char[262144];
+	ifstream myFile("./test.bin", std::ios::in | std::ios::binary);
+	if (!myFile.read(tempBuffer, 262144)) {
+		std::cerr << "Reading file failed: " << std::endl;
+		std::cerr << "read file status: " << myFile.gcount();
+	}
+	for (int i = 0; i < 10000; i++)
+		std::cout << " " << (int)tempBuffer[i];
+	myFile.close();
+	*/
 	std::cout << argc << std::endl;
 	init();
 	if (argc > 1) {
@@ -35,5 +46,7 @@ int main(int argc, char** argv) {
 		myfile << output_string << "\n";
 		myfile.close();
 	}
+	
+	
 	return 0;
 }
