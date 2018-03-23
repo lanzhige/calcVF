d14040c260a77baf71cb5f3f66d3bb787097369a
Lei Zhang (Student) <lzhan253@sundevils.asu.edu>
2018-03-23 14:47:31 -0700

    change doc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calcSVF
+run install.sh to install executable program under currenct directory.
+
+Instruction of use:
+calcSVF.exe BINARY_FILE_FOLDER_DIR
+
+need cmake and c++17 support for compiling the source code
--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+git clone git@github.com:lanzhige/calcSVF.git
+cd ./calcSVF
 mkdir -p ./build
-cd build
+cd ./build
 cmake ..
-cmake --build .
+cmake --build . --config Release
+cd ..
+cd ..
+cp ./calcSVF/build/src/Release/calcSVF.exe ./
+cp ./calcSVF/bin/zlib.dll ./
+cp ./calcSVF/README.md ./
+rm -rf ./calcSVF
