7dcc1bd126dca774a8e35a414123d9e18abe0496
dell xps 15 <zleizju@gmail.com>
2018-03-28 13:19:02 -0700

    change output

--- src/calcsvf.cpp
+++ src/calcsvf.cpp
@@ -6,7 +6,22 @@
 int main(int argc, char** argv) {
 	std::cout << argc << std::endl;
 	init();
-	if (argc > 1) {
+	if (argc > 2) {
+		std::cout << "input directory: " << argv[0] << std::endl;
+		string output_string = "";
+		recursiveLoad(argv[1], argv[1], output_string);
+		std::ofstream myfile;
+		string outname(argv[2]);
+		if (outname.find(".csv") == string::npos) {
+			outname = outname + ".csv";
+		}
+		myfile.open(&outname[0]);
+		myfile << "tileX,tileY,lat,lon,SVF";
+		myfile << output_string << "\n";
+		myfile.close();
+		std::cout << std::endl;
+	}
+	else if (argc > 1) {
 		std::cout << "input directory: "<< argv[0] << std::endl;
 		string output_string = "";
 		recursiveLoad(argv[1], argv[1], output_string);
